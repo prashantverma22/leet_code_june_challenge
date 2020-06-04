561a944effb9cd9f874936d6a40a42b2f9b89205
prashantverma22 <prashantverma.vn@gmail.com>
2020-06-04 19:11:44 +0530

    Add files via upload

---
+++ day 4/code.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+void reverse(char s[]){
+    int low=0;  //pointer for leftmost end
+    int high=strlen(s)-1; //pointer for rightmost end
+    while(low<high){
+        //swap until the string gets revered
+        swap(s[low],s[high]);
+        low++;
+        high--;
+    }
+}
+int main()
+{
+    char s[100];
+    cin>>s;
+    reverse(s);
+    for(int i=0;i<strlen(s);i++){
+        cout<<s[i]<<" ";
+    }
+    return 0;
+}
